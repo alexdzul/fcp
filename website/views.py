e75c1993c246cd4dcc057359565ca214e6f33760
Citlali Anali Galaviz Dzul <estilos1997@gmail.com>
2019-02-28 19:34:57 -0600

    html de contact y view
    se agregó correctamente la solicitud del formulario

--- website/views.py
+++ website/views.py
@@ -2,30 +2,27 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-
+from .models import Contact
 
 class Index(View):
 
     def get(self, request):
         return render(request, 'index.html')
 
+class ContactView(View):
 
-def contact (request):
-    ctx = {
-        'email': None,
-        'form': ContactForm()
-    }
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            mensaje = form.cleaned_data.get('mensaje')
-            ctx[
-                'mensaje'] = f'se obtuvieron los datos correctamente desde el servidor. Email {email}, Mensaje: {mensaje}'
-        else:
-            ctx['mensaje'] = 'Por favor, corrige tus errores'
-            ctx['form'] = form
-    return render(request, 'contact.html', ctx)
+    def get(self, request):
+        return render(request, 'contacto.html')
+
+    def post(self, request):
+        email = request.POST['email']
+        contact = Contact()
+        contact.email= email
+        contact.save()
+        ctx = {
+            'save': True
+        }
+        return render(request, 'contacto.html', ctx)
 
 class Quienes(View):
         def get(self, request):
