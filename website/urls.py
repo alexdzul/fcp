7fdcde9c75022253a64a821434c9cf98e1d8b01d
KUwill <48072228+KUwill@users.noreply.github.com>
2019-02-28 18:41:32 -0600

    Vista,url,
    Se realiza la Vista y el url de quienes-somos

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('quienes-somos/', views.Quienes.as_view(), name='quienes'),
     ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
 
 class Index(View):
 
     def get(self, request):
         return render(request, 'index.html')
+
+class Quienes(View):
+        def get(self, request):
+            return render(request, 'quienessomos.html')
