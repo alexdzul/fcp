6fdaccbc309fcc1e6b9482824515019b7146e83c
KUwill <48072228+KUwill@users.noreply.github.com>
2019-02-28 19:09:39 -0600

    Whoweare registrado en admin
    Se creo el modelo Whowear registrado en admin.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Whoweare
 
 # Register your models here.
+@admin.register(Whoweare)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('content',)
--- website/models.py
+++ website/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class email(models.Model):
     email = models.EmailField(max_length=70, blank=True)
+
+class Whoweare(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField(max_length=1000)
