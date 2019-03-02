a8dcff0659aea39cf8f1eb83bf96a88701a3a40a
Citlali Anali Galaviz Dzul <estilos1997@gmail.com>
2019-03-01 18:39:03 -0600

    se elimino requerimientos
    se eliminó archivo requerimientos xl

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 
-urlpatterns = {
+urlpatterns = [
     path('<int:product_id>/', views.detail, name='detail'),
     path('', views.Index.as_view(), name='index'),
     path('contacto/', views.ContactView.as_view(), name='contacto'),
     path('quienes-somos/', views.Quienes.as_view(), name='quienes'),
     path('nuestra-historia/', views.Historia.as_view(), name='historia')
-}
+]
